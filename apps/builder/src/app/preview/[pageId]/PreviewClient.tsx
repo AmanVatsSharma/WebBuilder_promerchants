decb1402e6360167cd1115ebd7099f74bafdea5b
AmanVatsSharma <aman95026@gmail.com>
2021-11-16 13:33:32 +0530

    Add storefront application with initial configuration, global styles, and basic API. Include layout and page components, along with testing setup and Cypress end-to-end tests for functionality verification.

--- apps/builder/src/app/preview/[pageId]/PreviewClient.tsx
+++ apps/builder/src/app/preview/[pageId]/PreviewClient.tsx
@@ -1,27 +1,31 @@
 /**
- * @file PreviewClient.tsx
- * @module preview
- * @description Client-side preview logic
- * @author BharatERP
- * @created 2025-02-09
+ * File: apps/builder/src/app/preview/[pageId]/PreviewClient.tsx
+ * Module: builder-preview
+ * Purpose: Client-side page preview (renders PageContentV1)
+ * Author: Cursor / Aman
+ * Last-updated: 2025-12-16
+ * Notes:
+ * - Uses same renderer as editor, but without selection/edit overlays
  */
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { PageRenderer, registerCoreComponents, PageContent } from '@web-builder/builder-core';
+import { PageRenderer, registerCoreComponents } from '@web-builder/builder-core';
+import type { PageContentV1 } from '@web-builder/contracts';
 
 registerCoreComponents();
 
 export default function PreviewClient({ pageId }: { pageId: string }) {
-  const [content, setContent] = useState<PageContent | null>(null);
+  const [content, setContent] = useState<PageContentV1 | null>(null);
 
   useEffect(() => {
+    console.debug('[builder-preview] loading page', { pageId });
     fetch(`/api/sites/pages/${pageId}`)
       .then(res => res.json())
       .then(data => {
-        if (data && data.content) setContent({ root: data.content });
+        if (data && data.content) setContent({ schemaVersion: 1, root: data.content });
       })
-      .catch(err => console.error(err));
+      .catch(err => console.error('[builder-preview] load failed', err));
   }, [pageId]);
 
   if (!content) return <div>Loading...</div>;
