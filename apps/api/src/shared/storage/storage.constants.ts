efb84d902f6f7b01ee54ed326017c067fe0b4206
AmanVatsSharma <aman95026@gmail.com>
2021-12-02 11:42:00 +0530

    Add Local Filesystem Storage Provider Implementation
    Introduce a new LocalFsStorageProvider for local filesystem storage, including methods for writing, reading, and listing files. Implement safety checks against path traversal. Add storage constants and types for better integration and type safety.

---
+++ apps/api/src/shared/storage/storage.constants.ts
@@ -0,0 +1,11 @@
+/**
+ * File: apps/api/src/shared/storage/storage.constants.ts
+ * Module: shared/storage
+ * Purpose: DI tokens and constants for storage providers
+ * Author: Cursor / Aman
+ * Last-updated: 2025-12-16
+ */
+
+export const STORAGE_PROVIDER = Symbol('STORAGE_PROVIDER');
+
+
---
+++ apps/api/src/shared/storage/storage.types.ts
@@ -0,0 +1,25 @@
+/**
+ * File: apps/api/src/shared/storage/storage.types.ts
+ * Module: shared/storage
+ * Purpose: StorageProvider interface (local fs in dev; S3 compatible in prod)
+ * Author: Cursor / Aman
+ * Last-updated: 2025-12-16
+ * Notes:
+ * - All keys must be treated as untrusted input; provider should defend against traversal
+ */
+
+export interface StorageWriteResult {
+  size: number;
+  sha256: string;
+}
+
+export interface StorageProvider {
+  ensurePrefix(prefix: string): Promise<void>;
+  writeBytes(key: string, content: Buffer): Promise<StorageWriteResult>;
+  writeText(key: string, content: string): Promise<StorageWriteResult>;
+  readText(key: string): Promise<string>;
+  exists(key: string): Promise<boolean>;
+  list(prefix: string): Promise<string[]>;
+}
+
+
