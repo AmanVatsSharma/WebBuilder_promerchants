cda3b021b92b7db0b2354d98397f39709955d560
AmanVatsSharma <aman95026@gmail.com>
2021-12-10 20:28:47 +0530

    Enhance Theme SDK with Public Exports and New Hooks
    - Updated index.ts to include public exports for types, hooks, and components.
    - Introduced hooks.ts for convenience hooks (useSite, useProducts, useCart) to simplify theme author interactions.
    - Expanded theme-sdk.tsx to implement a Theme SDK provider with a DevCommerceAdapter for managing site, products, and cart state.
    - Added types.ts to define shared types for ThemeSite, ThemeProduct, ThemeCart, and CommerceAdapter, laying groundwork for future commerce model expansions.

--- libs/theme-sdk/src/index.ts
+++ libs/theme-sdk/src/index.ts
@@ -1,2 +1,14 @@
+/**
+ * File: libs/theme-sdk/src/index.ts
+ * Module: theme-sdk
+ * Purpose: Public exports for theme authors (allowed import surface)
+ * Author: Cursor / Aman
+ * Last-updated: 2025-12-16
+ * Notes:
+ * - Theme bundles are compiled with an import allowlist that includes this package
+ */
 
 export * from './lib/theme-sdk';
+export * from './lib/types';
+export * from './lib/hooks';
+export * from './lib/components';
--- libs/theme-sdk/src/lib/theme-sdk.tsx
+++ libs/theme-sdk/src/lib/theme-sdk.tsx
@@ -1,3 +1,102 @@
+/**
+ * File: libs/theme-sdk/src/lib/theme-sdk.tsx
+ * Module: theme-sdk
+ * Purpose: Theme SDK provider + baseline components for ecommerce themes
+ * Author: Cursor / Aman
+ * Last-updated: 2025-12-16
+ * Notes:
+ * - This is the ONLY stable surface area theme bundles should import
+ * - In the future, builder/storefront will provide a real CommerceAdapter implementation
+ */
+
+import React, { createContext, useMemo, useState, useEffect } from 'react';
+import type { CommerceAdapter, ThemeCart, ThemeProduct, ThemeSite } from './types';
+
+export interface ThemeSdkContextValue {
+  site: ThemeSite | null;
+  products: ThemeProduct[];
+  cart: ThemeCart | null;
+  commerce: CommerceAdapter;
+  refresh(): Promise<void>;
+}
+
+const ThemeSdkContext = createContext<ThemeSdkContextValue | null>(null);
+
+class DevCommerceAdapter implements CommerceAdapter {
+  async getSite(): Promise<ThemeSite> {
+    return { id: 'dev', name: 'Dev Store', domain: null, currency: 'USD' };
+  }
+  async listProducts(): Promise<ThemeProduct[]> {
+    return [
+      {
+        id: 'p1',
+        title: 'Demo Product',
+        handle: 'demo-product',
+        priceCents: 1299,
+        currency: 'USD',
+        imageUrl: 'https://picsum.photos/seed/demo/800/600',
+      },
+    ];
+  }
+  async getCart(): Promise<ThemeCart> {
+    return { lines: [], currency: 'USD' };
+  }
+  async addToCart(): Promise<ThemeCart> {
+    return { lines: [], currency: 'USD' };
+  }
+}
+
+export function ThemeSdkProvider({
+  children,
+  commerce,
+}: {
+  children: React.ReactNode;
+  commerce?: CommerceAdapter;
+}) {
+  const adapter = commerce ?? new DevCommerceAdapter();
+  const [site, setSite] = useState<ThemeSite | null>(null);
+  const [products, setProducts] = useState<ThemeProduct[]>([]);
+  const [cart, setCart] = useState<ThemeCart | null>(null);
+
+  const refresh = async () => {
+    try {
+      console.debug('[theme-sdk] refresh');
+      const [s, p, c] = await Promise.all([adapter.getSite(), adapter.listProducts(), adapter.getCart()]);
+      setSite(s);
+      setProducts(p);
+      setCart(c);
+    } catch (e) {
+      console.error('[theme-sdk] refresh failed', e);
+    }
+  };
+
+  useEffect(() => {
+    void refresh();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const value = useMemo<ThemeSdkContextValue>(
+    () => ({
+      site,
+      products,
+      cart,
+      commerce: adapter,
+      refresh,
+    }),
+    [site, products, cart, adapter],
+  );
+
+  return <ThemeSdkContext.Provider value={value}>{children}</ThemeSdkContext.Provider>;
+}
+
+export function useThemeSdk() {
+  const ctx = React.useContext(ThemeSdkContext);
+  if (!ctx) {
+    throw new Error('ThemeSdkProvider is missing. Wrap your theme in <ThemeSdkProvider>.');
+  }
+  return ctx;
+}
+
 
 import styles from './theme-sdk.module.css';
 
