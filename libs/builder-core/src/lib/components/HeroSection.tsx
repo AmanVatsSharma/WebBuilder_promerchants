b5656b7b19511f7103a0a40756d4b3a7bf9227e1
AmanVatsSharma <aman95026@gmail.com>
2022-05-11 16:11:49 +0530

    Enhance HeroSection Component with Background Image Support
    - Updated HeroSection component to accept an optional backgroundImageUrl prop, allowing for dynamic background images.
    - Implemented inline styles to apply a gradient overlay and background image when the prop is provided.
    - Modified manifest.json to include a new media field for background image selection, improving customization options for the theme.

--- libs/builder-core/src/lib/components/HeroSection.tsx
+++ libs/builder-core/src/lib/components/HeroSection.tsx
@@ -7,9 +7,21 @@
  */
 import React from 'react';
 
-export const HeroSection: React.FC<{ title: string; subtitle?: string }> = ({ title, subtitle }) => {
+export const HeroSection: React.FC<{ title: string; subtitle?: string; backgroundImageUrl?: string }> = ({
+  title,
+  subtitle,
+  backgroundImageUrl,
+}) => {
+  const style: React.CSSProperties | undefined = backgroundImageUrl
+    ? {
+        backgroundImage: `linear-gradient(rgba(37,99,235,0.75), rgba(37,99,235,0.75)), url(${backgroundImageUrl})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }
+    : undefined;
+
   return (
-    <div className="bg-blue-600 text-white py-20 px-8 text-center">
+    <div className="bg-blue-600 text-white py-20 px-8 text-center" style={style}>
       <h1 className="text-4xl font-bold mb-4">{title}</h1>
       {subtitle && <p className="text-xl">{subtitle}</p>}
     </div>
