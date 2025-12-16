64942cee1d2a6cfb8c5ddac652403112832850ae
AmanVatsSharma <aman95026@gmail.com>
2025-12-16 15:44:11 +0530

    Add Theme SDK Library
    - Introduced a new Theme SDK library with essential configurations including Babel, ESLint, Jest, and TypeScript settings.
    - Created initial components and styles for the Theme SDK, including a sample component and its corresponding test.
    - Updated tsconfig.base.json to include the new library path.

---
+++ libs/theme-sdk/src/lib/theme-sdk.tsx
@@ -0,0 +1,15 @@
+
+import styles from './theme-sdk.module.css';
+
+export function ThemeSdk() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to ThemeSdk!</h1>
+    </div>
+  );
+}
+
+export default ThemeSdk;
+
+
+
